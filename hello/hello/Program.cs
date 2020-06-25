bbcb397e0827ad538f7a7378f2b5c88f48ef8164
zyx25 <zyx1725789213@outlook.com>
2020-06-25 10:58:50 +0800

    my first project

---
+++ hello/hello/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int a = 1;
+            int b = 2;
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
